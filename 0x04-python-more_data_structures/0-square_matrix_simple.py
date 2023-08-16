b4a527509183e388311690548ee10222e246b813
zwambui <wambuinganga.wn@gmail.com>
2023-08-16 07:55:08 -0700

    more data structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+computes the square value of all integers of a matrix.
+"""
+
+
+def square_matrix_simple(matrix=[]):
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a function that replaces all occurrences
+of an element by another in a new list.
+"""
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
