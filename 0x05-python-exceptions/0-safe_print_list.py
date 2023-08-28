eb009d705b9d1f5971c3da90ca614506e0037952
zwambui <wambuinganga.wn@gmail.com>
2023-08-28 07:50:58 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ prints x elements of a list.
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    elements = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        except Exception:
+            pass
+    print()
+    return elements
