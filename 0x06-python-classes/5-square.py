a8f27fcade36ba28ce7630f6bb2eeff4fa38ef19
zwambui <wambuinganga.wn@gmail.com>
2023-08-30 07:35:34 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """represents a square"""
+
+    def __init__(self, size=0):
+        """initialize a new square
+        args:
+        size (int): Size of the new square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """set current size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set property of the square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns current square area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """print the square with # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
