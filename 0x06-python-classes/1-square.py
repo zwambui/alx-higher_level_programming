2c9ea6476dd71466f3e388565539790120426785
zwambui <wambuinganga.wn@gmail.com>
2023-08-30 07:27:51 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """initialize a new square
+        args:
+        size (int): Size of the new square.
+        """
+        self.__size = size
