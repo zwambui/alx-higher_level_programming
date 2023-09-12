97996bf83d1dcfe575b498d7cf90daf74ea2444b
zwambui <wambuinganga.wn@gmail.com>
2023-09-12 10:29:58 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""student to json module"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initializes student class
+        args:
+            first_name: students first name
+            last_name: students last name
+            age(int): student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
